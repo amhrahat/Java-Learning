8450675a3db9aa1e690c51314cb4cf76a60e51a5
“Abdullah Rahat” <rahatabdullah879@gmail.com>
2026-01-14 12:03:35 +0600

    commiting methods

---
+++ w3school/JavaMethods.java
@@ -0,0 +1,11 @@
+public class JavaMethods{
+
+    static void method(){
+        System.out.println("Printing from method");
+    }
+    
+    public static void main (String[] args) {
+        method();
+
+    }
+}
